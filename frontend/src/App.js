b24066c130081f541c02604ac0962ba587ad8289
joshR1234 <josh.romley@gmail.com>
2023-08-08 15:48:07 -0400

    adding image resources to project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
-import { Container } from "react-bootstrap";
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
-import Footer from "./components/Footer";
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen'; // new
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className="py-3">
+      <main className='py-3'>
         <Container>
-          <h1>Welcome to Proshop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
